479b27364541126184d2e11b876cd749f514abab
imantalebizadeh <imantalebizadeh01@gmail.com>
2024-02-01 18:51:03 +0330

    feat: Provide Toaster component

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@/context/ThemeProvider.tsx";
+import { Toaster } from "@/components/ui/toaster.tsx";
 import App from "./App.tsx";
 import "./index.css";
-import { ThemeProvider } from "./context/ThemeProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="theme">
       <App />
+      <Toaster />
     </ThemeProvider>
   </React.StrictMode>,
 );
