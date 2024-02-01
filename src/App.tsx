0f5af9c5f3fdd7cafac740b3d8a3af35b314f7e9
imantalebizadeh <imantalebizadeh01@gmail.com>
2024-02-01 18:59:29 +0330

    feat; Add the AddForm component to the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Header from "@/components/Header";
+import AddForm from "@/components/AddForm";
 
 export default function App() {
   return (
-    <div className="mx-auto h-screen w-full max-w-screen-sm">
+    <div className="mx-auto flex h-screen w-full max-w-screen-sm flex-col gap-8">
+      {/* Header component */}
       <Header />
+
+      {/* AddForm component */}
+      <AddForm />
     </div>
   );
 }
