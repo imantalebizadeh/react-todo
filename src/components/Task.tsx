1a7db7c97bfa8610a5057a9cf4be90349c6ae214
imantalebizadeh <imantalebizadeh01@gmail.com>
2024-02-04 19:38:56 +0330

    feat: Create a component to display each task

---
+++ src/components/Task.tsx
@@ -0,0 +1,26 @@
+import { Task as TaskType } from "@/types/Tasks";
+import { TableCell, TableRow } from "./ui/table";
+import { Checkbox } from "./ui/checkbox";
+import { ComponentProps, Ref } from "react";
+import TaskActions from "./TaskActions";
+
+type Props = {
+  task: TaskType;
+  ref?: Ref<HTMLTableRowElement>;
+} & ComponentProps<"tr">;
+
+export default function Task({ task, ...props }: Props) {
+  return (
+    <TableRow {...props}>
+      <TableCell>
+        <Checkbox className="rounded-md" />
+      </TableCell>
+      <TableCell>{task.name}</TableCell>
+      <TableCell>{task.status}</TableCell>
+      <TableCell>{task.priority}</TableCell>
+      <TableCell>
+        <TaskActions />
+      </TableCell>
+    </TableRow>
+  );
+}
