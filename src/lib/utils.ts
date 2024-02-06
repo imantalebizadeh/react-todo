09f07956b04f69a02313b5a4b6a49171c8c8b98b
imantalebizadeh <imantalebizadeh01@gmail.com>
2024-02-06 22:48:06 +0330

    feat: Create a utility function to capitalize strings

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function capitalize(value: string) {
+  if (value.includes(" ")) {
+    const splittedString = value.split(" ");
+
+    return splittedString
+      .map((value) => value.charAt(0).toUpperCase() + value.slice(1))
+      .join(" ");
+  }
+
+  return value.charAt(0).toUpperCase() + value.slice(1);
 }
