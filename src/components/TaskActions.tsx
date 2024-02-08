3882dfa050d3875257a940a4b27ed651de333d53
imantalebizadeh <imantalebizadeh01@gmail.com>
2024-02-08 22:55:20 +0330

    feat: Handle modal open/close state

--- src/components/TaskActions.tsx
+++ src/components/TaskActions.tsx
@@ -9,14 +9,17 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import { MoreHorizontal } from "lucide-react";
+import { useState } from "react";
 
 export default function TaskActions() {
+  const [open, setOpen] = useState(false);
+
   const handleRemove = () => {
     alert("remove");
   };
 
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DropdownMenu>
         <DropdownMenuTrigger>
           <MoreHorizontal className="h-5 w-5" />
@@ -32,7 +35,7 @@ export default function TaskActions() {
       </DropdownMenu>
 
       {/* Modal content */}
-      <ModalContent title="Edit your task"/>
+      <ModalContent title="Edit your task" setOpen={setOpen} />
     </Dialog>
   );
 }
