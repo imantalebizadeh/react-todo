d25b9e7f618ab3c69c41db0f80f392b2419c7d47
imantalebizadeh <imantalebizadeh01@gmail.com>
2024-02-09 19:20:48 +0330

    feat: Add id property to Task type

--- src/types/Tasks.ts
+++ src/types/Tasks.ts
@@ -1,7 +1,7 @@
 import { EditFormSchema } from "@/lib/vlidations";
 import { z } from "zod";
 
-export type Task = z.infer<typeof EditFormSchema>;
+export type Task = { id: string } & z.infer<typeof EditFormSchema>;
 export type TaskStatus = z.infer<typeof EditFormSchema.shape.status>;
 export type TaskPriority = z.infer<typeof EditFormSchema.shape.priority>;
 
