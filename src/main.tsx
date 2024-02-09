54ac02387b71c9aae7e58bb1300ce5b554fc5a33
imantalebizadeh <imantalebizadeh01@gmail.com>
2024-02-09 18:58:11 +0330

    feat: Provide persist gate

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,20 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@/context/ThemeProvider.tsx";
 import { Toaster } from "@/components/ui/toaster.tsx";
 import { Provider } from "react-redux";
-import store from "@/store";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistedStore } from "@/store";
 import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider defaultTheme="system" storageKey="theme">
-        <App />
-        <Toaster />
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistedStore}>
+        <ThemeProvider defaultTheme="system" storageKey="theme">
+          <App />
+          <Toaster />
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
