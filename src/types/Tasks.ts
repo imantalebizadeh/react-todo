18e0052227be333a150c4163ed58275c7c58bfa6
imantalebizadeh <imantalebizadeh01@gmail.com>
2024-02-10 18:48:14 +0330

    feat: Add "completed" property to Task type

--- src/types/Tasks.ts
+++ src/types/Tasks.ts
@@ -1,6 +1,8 @@
 import { EditFormSchema } from "@/lib/vlidations";
 import { z } from "zod";
 
-export type Task = { id: string } & z.infer<typeof EditFormSchema>;
+export type Task = { id: string; completed: boolean } & z.infer<
+  typeof EditFormSchema
+>;
 export type TaskStatus = z.infer<typeof EditFormSchema.shape.status>;
 export type TaskPriority = z.infer<typeof EditFormSchema.shape.priority>;
