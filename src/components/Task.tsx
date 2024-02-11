3e87dab9fdd1a8ebc8c59360892df6186d5647a5
imantalebizadeh <imantalebizadeh01@gmail.com>
2024-02-11 09:41:37 +0330

    feat: Implement the functionality of changing the status of the task to completed

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,9 +1,12 @@
 import { Task as TaskType } from "@/types/Tasks";
 import { TableCell, TableRow } from "./ui/table";
 import { Checkbox } from "./ui/checkbox";
-import { ComponentProps, Ref } from "react";
+import { ComponentProps, Ref, useEffect, useState } from "react";
 import TaskActions from "./TaskActions";
-import { splitCapitalizedWord } from "@/lib/utils";
+import { cn, splitCapitalizedWord } from "@/lib/utils";
+import { useAppDispatch } from "@/hooks/useAppDispatch";
+import { selectTaskById, toggleComplete } from "@/reducers/tasksSlice";
+import { useAppSelector } from "@/hooks/useAppSelector";
 
 type Props = {
   task: TaskType;
@@ -11,12 +14,39 @@ type Props = {
 } & ComponentProps<"tr">;
 
 export default function Task({ task, ...props }: Props) {
+  const [checked, setChecked] = useState<boolean>(false);
+
+  const { completed } = useAppSelector((state) =>
+    selectTaskById(state, task.id),
+  ) as TaskType;
+
+  useEffect(() => {
+    if (completed) {
+      setChecked(true);
+    } else {
+      setChecked(false);
+    }
+  }, [completed]);
+
+  const dispatch = useAppDispatch();
+
+  const handleToggle = () => {
+    setChecked((prevState) => !prevState);
+    dispatch(toggleComplete({ taskId: task.id }));
+  };
+
   return (
     <TableRow {...props}>
       <TableCell>
-        <Checkbox className="rounded-md" />
+        <Checkbox
+          className="rounded-md"
+          checked={checked}
+          onCheckedChange={handleToggle}
+        />
       </TableCell>
-      <TableCell>{task.title}</TableCell>
+      <TableCell className={cn({ "line-through": checked })}>
+        {task.title}
+      </TableCell>
       <TableCell>{splitCapitalizedWord(task.status)}</TableCell>
       <TableCell>{task.priority}</TableCell>
       <TableCell>
